d51f3ac18e0bfda9bfe6ed9b0e40a9e8742eefb3
Mohd Robiul Alam <72224033+mohdrony@users.noreply.github.com>
2025-12-29 11:42:36 +0000

    styling for active pages changed

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,67 +1,78 @@
 import "./sidebar.scss";
+import { NavLink } from "react-router-dom";
 
-import DashboardCustomizeIcon from '@mui/icons-material/DashboardCustomize';
-import ViewTimelineIcon from '@mui/icons-material/ViewTimeline';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import AccountTreeIcon from '@mui/icons-material/AccountTree';
-import GroupIcon from '@mui/icons-material/Group';
-import DisplaySettingsIcon from '@mui/icons-material/DisplaySettings';
-import { Link } from "react-router-dom";
+import DashboardCustomizeIcon from "@mui/icons-material/DashboardCustomize";
+import ViewTimelineIcon from "@mui/icons-material/ViewTimeline";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
+import GroupIcon from "@mui/icons-material/Group";
+import DisplaySettingsIcon from "@mui/icons-material/DisplaySettings";
+
+const navClass = ({ isActive }) => (isActive ? "navLink active" : "navLink");
 
 const Sidebar = () => {
-    return (
-        <div className="sidebar">
-            <div className="top">
-                <Link to="/" style={{ textDecoration: "none" }}>
-                    <span className="logo">urbanegestalt</span>
-                </Link>
-            </div>
-            <hr />
-            <div className="center">
-                <ul>
-                    <p className="title">MAIN</p>
-                    <Link to="/" style={{ textDecoration: "none" }}>
-                        <li>
-                            <DashboardCustomizeIcon className="icon" />
-                            <span>Dashboard</span>
-                        </li>
-                    </Link>
-                    <p className="title">BACKOFFICE</p>
-                    <Link to="/timeline" style={{ textDecoration: "none" }}>
-                        <li>
-                            <ViewTimelineIcon className="icon" />
-                            <span>Timeline</span>
-                        </li>
-                    </Link>
-                    <Link to="/calendar" style={{ textDecoration: "none" }}>
-                        <li>
-                            <CalendarMonthIcon className="icon" />
-                            <span>Calendar</span>
-                        </li>
-                    </Link>
-                    <Link to="/projects" style={{ textDecoration: "none" }}>
-                        <li>
-                            <AccountTreeIcon className="icon" />
-                            <span>Projects</span>
-                        </li>
-                    </Link>
-                    <p className="title">MANAGE</p>
-                    <li>
-                        <GroupIcon className="icon" />
-                        <span>Team</span>
-                    </li>
-                    <li>
-                        <DisplaySettingsIcon className="icon" />
-                        <span>Settings</span>
-                    </li>
-                </ul>
-            </div>
-            <div className="bottom">
-                <div className="colorOption"></div>
-                <div className="colorOption"></div>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <NavLink to="/" className="logoLink">
+          <span className="logo">urbanegestalt</span>
+        </NavLink>
+      </div>
+
+      <hr />
+
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <NavLink to="/" className={navClass} end>
+              <DashboardCustomizeIcon className="icon" />
+              <span>Dashboard</span>
+            </NavLink>
+          </li>
+
+          <p className="title">BACKOFFICE</p>
+          <li>
+            <NavLink to="/timeline" className={navClass}>
+              <ViewTimelineIcon className="icon" />
+              <span>Timeline</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/calendar" className={navClass}>
+              <CalendarMonthIcon className="icon" />
+              <span>Calendar</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/projects" className={navClass}>
+              <AccountTreeIcon className="icon" />
+              <span>Projects</span>
+            </NavLink>
+          </li>
+
+          <p className="title">MANAGE</p>
+          <li>
+            <NavLink to="/team" className={navClass}>
+              <GroupIcon className="icon" />
+              <span>Team</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/settings" className={navClass}>
+              <DisplaySettingsIcon className="icon" />
+              <span>Settings</span>
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+
+      <div className="bottom">
+        <div className="colorOption" />
+        <div className="colorOption" />
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
